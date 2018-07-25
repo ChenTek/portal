9809a7dcd4f32f7063ca0816d29bb9db11a8e5c8
ChenTek <chen@tekweld.com>
2018-07-25 13:47:31 -0400

    sf

--- src/app/_helpers/globals.ts
+++ src/app/_helpers/globals.ts
@@ -1,4 +1,4 @@
 export class Globals {
-  public baseURL = 'http://localhost:8081/tekweld/jwtservices';
-  //public baseURL = 'http://tekweld.xy8imwiyvs.us-east-2.elasticbeanstalk.com/jwtservices';
+ // public baseURL = 'http://localhost:8081/tekweld/jwtservices';
+  public baseURL = 'http://tekweld.xy8imwiyvs.us-east-2.elasticbeanstalk.com/jwtservices';
 }
