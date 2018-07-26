d283d857858d5ec525854dfcfdb26342e2341a07
ChenTek <chen@tekweld.com>
2018-07-26 16:14:21 -0400

    habga

--- src/app/_helpers/globals.ts
+++ src/app/_helpers/globals.ts
@@ -1,4 +1,5 @@
 export class Globals {
  // public baseURL = 'http://localhost:8081/tekweld/jwtservices';
-  public baseURL = 'http://tekweld.xy8imwiyvs.us-east-2.elasticbeanstalk.com/jwtservices';
+ // public baseURL = 'http://tekweld.xy8imwiyvs.us-east-2.elasticbeanstalk.com/jwtservices';
+  public baseURL = 'https://be.tektests.com/jwtservices';
 }
